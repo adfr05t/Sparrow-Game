1ea0f5e37b4c98f0aabd256b1a3df6dd7cd520e0
Adam Frost <78973950+adfr05t@users.noreply.github.com>
2023-04-03 20:51:07 +0100

    Alter SparrowMovement script so that player movement occurs relative to current position of freelook camera

--- Assets/Scripts/SparrowMovement.cs
+++ Assets/Scripts/SparrowMovement.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class SparrowMovement : MonoBehaviour
 {
+  //  [SerializeField] private Camera theCamera;
     [SerializeField] private float moveSpeed;
     private float horizontalInput;
     private float verticalInput;
@@ -33,9 +34,37 @@ public class SparrowMovement : MonoBehaviour
     {
         Vector2 inputVector = new Vector2(horizontalInput, verticalInput);   
         inputVector = Vector2.ClampMagnitude(inputVector, 1); // w/o this, speed would be greater on diagonal 
-        body.velocity = new Vector3(inputVector.x * moveSpeed, body.velocity.y, inputVector.y * moveSpeed);
+
+        Vector3 movementRelativeToCamera = ConvertToCameraSpace(inputVector);
+
+        body.velocity = new Vector3(movementRelativeToCamera.x * moveSpeed, body.velocity.y, movementRelativeToCamera.z * moveSpeed);
         // Orient transform's forward vector with movement direction
-        transform.LookAt(transform.position + new Vector3(horizontalInput, 0, verticalInput));
+        transform.LookAt(transform.position + new Vector3(movementRelativeToCamera.x, 0, movementRelativeToCamera.z));
+    }
+
+
+    Vector3 ConvertToCameraSpace(Vector2 playerInput)
+    {
+        // Make input vector into a Vector3 (input y will become z)
+        Vector3 vectorToRotate = new Vector3(playerInput.x, 0, playerInput.y);
+
+        Vector3 cameraForward = Camera.main.transform.forward;
+        Vector3 cameraRight = Camera.main.transform.right;
+
+        // Ignore y components
+        cameraForward.y = 0;
+        cameraRight.y = 0;
+        
+        cameraForward = cameraForward.normalized;
+        cameraRight = cameraRight.normalized;
+
+        // Rotate the x and z vectors using cam directional vectors
+        Vector3 cameraForwardZProduct = vectorToRotate.z * cameraForward;
+        Vector3 cameraRightXProduct = vectorToRotate.x * cameraRight;
+
+        Vector3 vectorRotatedToCameraSpace = cameraForwardZProduct + cameraRightXProduct;
+
+        return vectorRotatedToCameraSpace;
     }
 
     void UpdateAnimation()
